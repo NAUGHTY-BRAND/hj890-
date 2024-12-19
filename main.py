7ca43bec68ffd0d645283919c931423aa0436993
Faris XD <145090660+NAUGHTY-BRAND@users.noreply.github.com>
2024-12-19 23:51:56 +0500

    Create main.py

---
+++ main.py
@@ -0,0 +1,19 @@
+from flask import Flask, request, jsonify
+from meta_ai_api import MetaAI
+app = Flask(__name__)
+
+ai = MetaAI()
+
+@app.route('/metaAi', methods=['GET'])
+def meta_ai():
+    prompt = request.args.get('prompt', '')
+
+    if prompt:
+
+        response = ai.prompt(message=prompt)
+        return jsonify(response=response)
+    else:
+        return jsonify(error="No prompt provided"), 400
+
+if __name__ == '__main__':
+    app.run(host='localhost', port=3000)
